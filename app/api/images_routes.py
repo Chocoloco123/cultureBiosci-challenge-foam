5714dd207db7382fb62d35a0e6ac261a0205b2fa
Chocoloco123 <csarkki@gmail.com>
2022-03-05 23:14:03 -0800

    fixed backend pagination bug with .items

--- app/api/images_routes.py
+++ app/api/images_routes.py
@@ -9,12 +9,14 @@ images_routes = Blueprint('image', __name__)
 # @images_routes.route('/', methods=['GET'])
 # def get_images():
   
-@images_routes.route('/images/<int:pageNumber>', methods=['GET'])
+@images_routes.route('/<int:pageNumber>', methods=['GET'])
 def paginated_images(pageNumber):
   IMAGES_PER_PAGE = 20
-  images = Image.query.paginate(pageNumber, IMAGES_PER_PAGE, False);
+  images = Image.query.paginate(pageNumber, IMAGES_PER_PAGE, False)
+  print(images)
+  print('here!!!!!!!!')
   if images:
-    images = {image.id : image.to_dict() for image in images}
+    images = dict([(image.id, image.to_dict()) for image in images.items])
     return images
   else:
     return {'message' : 'No images found.'}
