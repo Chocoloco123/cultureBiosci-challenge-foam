6e33bed17f2ef95349f4c296664b99695e551b57
Chocoloco123 <csarkki@gmail.com>
2022-03-06 00:39:00 -0800

    added edit backend route

--- app/api/images_routes.py
+++ app/api/images_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request
 from app.models import Image, db
 from app.forms import FoamStatusForm
+from datetime import datetime
 
 from flask_login import current_user
 
@@ -21,5 +22,21 @@ def paginated_images(pageNumber):
     return images
   else:
     return {'message' : 'No images found.'}
+
+@images_routes.route('/<int:pageNumber>/<int:id>/update', methods=['GET', 'PATCH'])
+def update_image_status(pageNumber, id):
+  form = FoamStatusForm()
+  form['csrf_token'.data] = request.cookies('csrf_token')
+  image = Image.query.get(id)
+
+  if form.validate_on_submit():
+    image.url = form.data['url']
+    image.foamStatus = form.data['foamStatus']
+    image.lastModified = datetime.now()
+
+    db.session.commit()
+    return image.to_dict()
+  else:
+    return {"message" : "Unable to update foam status."}
 
 
