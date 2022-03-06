12549ae447d0b40e9bff266439e0785a806c52a5
Chocoloco123 <csarkki@gmail.com>
2022-03-05 20:38:26 -0800

    got default page to render

---
+++ react-app/src/store/images.js
@@ -0,0 +1,11 @@
+
+const initial_state = {};
+
+const imagesReducer = (state = initial_state, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default imagesReducer;
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+// import session from './session'
+import image from "./images"
 
 const rootReducer = combineReducers({
-  session,
+  image
 });
 
 
